6d21a060a3beefb9614639d1bb3bb4554b1f2358
aseem adkary <aseem@aseem.aseem.com>
2025-03-19 07:04:19 +0545

    menu changes width

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('frontend/image/logo-white-bg-circle.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('backend/css/dashboard-style.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/css/header-style.css') }}">
     @stack('title')
